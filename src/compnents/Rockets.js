03f971d29a3899e1107e806c8fa909a7c8f594af
Omwomo <eugeneoduor79@gmail.com>
2023-09-12 20:31:07 +0300

    create componet for rockets

--- src/compnents/Rockets.js
+++ src/compnents/Rockets.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-export default function Rockets() {
+const RocketsComponent = () => {
+  const rockets = useSelector((state) => state.rockets);
+
   return (
-    <h2>Hello Rockets</h2>
+    <div>
+      {rockets.map((rocket) => (
+        <div key={rocket.id}>
+          <h3>{rocket.rocket_name}</h3>
+          <p>{rocket.description}</p>
+          <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
+        </div>
+      ))}
+    </div>
   );
-}
+};
+
+export default RocketsComponent;
