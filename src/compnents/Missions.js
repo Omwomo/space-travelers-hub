6d2826cde1fb817e3013752c9dade77b84b2cc21
George <georgie.george7@gmail.com>
2023-09-12 16:11:54 +0400

    Basic structure

--- src/compnents/Missions.js
+++ src/compnents/Missions.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import '../styling/mission.css';
 
 export default function Missions() {
   return (
-    <h2>Hello Missions</h2>
+    <div className="missionTable">
+      <table>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th className="wider">Description</th>
+            <th>Status</th>
+            <th>&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>name</th>
+            <td>
+              description
+            </td>
+            <td>
+              NOT A MEMBER
+            </td>
+            <td>
+              <button type="button">
+                Join Mission
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
